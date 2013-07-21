05cc02a7e34c27f2b385da4b03484489a88e272a
Jasper Reddin <jazzbeenjamin@me.com>
2013-07-21 14:21:28 -0500

    Added initial difficulty setting
    I have now implemented the ability to set the difficulty before playing
    the game.
    
    Elementary Difficulty: adding two random numbers from 0 to 10
    Middle School Difficulty: adding two random numbers from 0 to 20
    High School Difficulty: adding two random numbers from -20 to 20
    
    NOTE: This is not an official release update to the game, it is just an
    update to the code. In time, this code will be built and put in an
    executable jar file and uploaded to SourceForge as an official release.

--- MathTest.java
+++ MathTest.java
@@ -5,6 +5,10 @@ import java.awt.event.KeyEvent;
 import java.awt.event.KeyListener;
 import java.awt.event.WindowAdapter;
 import java.awt.event.WindowEvent;
+import java.io.ObjectInputStream;
+import java.io.ObjectOutputStream;
+import java.net.ServerSocket;
+import java.net.Socket;
 import java.util.Random;
 import javax.swing.*;
 import javax.swing.text.BadLocationException;
@@ -19,6 +23,7 @@ import javax.swing.text.BadLocationException;
 public class MathTest extends JFrame {
 	
 	JScrollPane scrollPane;
+	JPanel inputPanel;
 	static JTextArea consoleMessages;
 	static JTextField input;
 	static int numberOfTimesPlayed;
@@ -30,7 +35,29 @@ public class MathTest extends JFrame {
 	static JButton enter;
 	static String LastLine1;
 	static String LastLine2;
-	JPanel inputPanel;
+	static int difficultyLevel;
+	
+	// This field stores what the program
+	// is waiting for when it waits for 
+	// user input.
+	static int state;
+	
+	// Difficulty fields
+	public static final int ELEMENTARY_DIFFICULTY = 0;
+	public static final int MIDDLE_SCHOOL_DIFFICULTY = 1;
+	public static final int HIGH_SCHOOL_DIFFICULTY = 2;
+	// State fields
+	public static final int VARIABLE_STATE = 0;
+	public static final int DIFFICULTY_CHANGING_STATE = 1;
+	
+	// These fields have been added for the local
+	// Multiplayer mode, but I don't think I will
+	// put that feature in quite yet.
+	/*ObjectOutputStream outputstream;
+	ObjectInputStream inputstream;
+	ServerSocket serverThing;
+	Socket connection;*/
+	
 	
 	public MathTest() {
 		super("Math Quiz");
@@ -69,12 +96,8 @@ public class MathTest extends JFrame {
 		    }
 		});
 		
-		numberOfTimesPlayed = 1;
-		EnterText("Question #" + numberOfTimesPlayed);
-		number1 = generator.nextInt(20);
-		number2 = generator.nextInt(20);
-		total = number1 + number2;
-		EnterText("What is " + number1 + " + " + number2 + "?");
+		EnterText("Please enter your difficulty: Elementary, Middle School, High School");
+		setQuestionState(DIFFICULTY_CHANGING_STATE);
 		
 		setResizable(false);
 		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
@@ -90,41 +113,99 @@ public class MathTest extends JFrame {
 		LastLine1 = input;
 		LastLine2 = LastLine1;
 	}
-
+	
+	public static void setDifficulty(int newDifficulty){
+		difficultyLevel = newDifficulty;
+	}
+	
+	public static void setQuestionState (int newState){
+		state = newState;
+	}
 
 	
 	public static void nextQuestion(int numberOne, int numberTwo, int answer){
 		if (numberOfTimesPlayed <= 9){
-			if (total == answer){
-				EnterText("You are correct!");
-				score += 10;
-				
-				numberOfTimesPlayed += 1;
-				EnterText("Question #" + numberOfTimesPlayed);
-				
-				number1 = generator.nextInt(20);
-				number2 = generator.nextInt(20);
-				total = number1 + number2;
-				
-				EnterText("What is " + number1 + " + " + number2 + "?");
-			} else {
-				EnterText("Wrong.");
-				
-				numberOfTimesPlayed += 1;
-				EnterText("Question #" + numberOfTimesPlayed);
-				
-				number1 = generator.nextInt(20);
-				number2 = generator.nextInt(20);
-				total = number1 + number2;
-				
-				EnterText("What is " + number1 + " + " + number2 + "?");
+			if (difficultyLevel == 0){
+				if (total == answer){
+					EnterText("You are correct!");
+					score += 10;
+
+					numberOfTimesPlayed += 1;
+					EnterText("Question #" + numberOfTimesPlayed);
+
+					number1 = generator.nextInt(11);
+					number2 = generator.nextInt(11);
+					total = number1 + number2;
+
+					EnterText("What is " + number1 + " + " + number2 + "?");
+				} else {
+					EnterText("Wrong.");
+
+					numberOfTimesPlayed += 1;
+					EnterText("Question #" + numberOfTimesPlayed);
+
+					number1 = generator.nextInt(11);
+					number2 = generator.nextInt(11);
+					total = number1 + number2;
+
+					EnterText("What is " + number1 + " + " + number2 + "?");
+				}
+			}else if (difficultyLevel == 1){
+				if (total == answer){
+					EnterText("You are correct!");
+					score += 10;
+
+					numberOfTimesPlayed += 1;
+					EnterText("Question #" + numberOfTimesPlayed);
+
+					number1 = generator.nextInt(21);
+					number2 = generator.nextInt(21);
+					total = number1 + number2;
+
+					EnterText("What is " + number1 + " + " + number2 + "?");
+				} else {
+					EnterText("Wrong.");
+
+					numberOfTimesPlayed += 1;
+					EnterText("Question #" + numberOfTimesPlayed);
+
+					number1 = generator.nextInt(21);
+					number2 = generator.nextInt(21);
+					total = number1 + number2;
+
+					EnterText("What is " + number1 + " + " + number2 + "?");
+				}
+			}else if (difficultyLevel == 2){
+				if (total == answer){
+					EnterText("You are correct!");
+					score += 10;
+
+					numberOfTimesPlayed += 1;
+					EnterText("Question #" + numberOfTimesPlayed);
+
+					number1 = generator.nextInt(41) - 20;
+					number2 = generator.nextInt(41) - 20;
+					total = number1 + number2;
+
+					EnterText("What is " + number1 + " + " + number2 + "?");
+				} else {
+					EnterText("Wrong.");
+
+					numberOfTimesPlayed += 1;
+					EnterText("Question #" + numberOfTimesPlayed);
+
+					number1 = generator.nextInt(41) - 20;
+					number2 = generator.nextInt(41) - 20;
+					total = number1 + number2;
+
+					EnterText("What is " + number1 + " + " + number2 + "?");
+				}
 			}
 		} else {
 			
 			if (total == answer){
 				EnterText("You are correct!");
 				score += 10;
-				System.out.println("Adding 10 to score. current score: " + score);
 			}else{
 				EnterText("Wrong.");
 			}
@@ -177,7 +258,7 @@ class enterlistener implements ActionListener, KeyListener{
 			MathTest.input.setEditable(false);
 			MathTest.enter.setEnabled(false);
 			
-		}else if(txt.equals("/clear")){
+		}else if(txt.toLowerCase().equals("/clear")){
 			MathTest.consoleMessages.setText("");
 			MathTest.input.selectAll();
 			MathTest.EnterText(MathTest.LastLine1); 
@@ -197,8 +278,40 @@ class enterlistener implements ActionListener, KeyListener{
 			MathTest.EnterText(txtForWrapTest);
 			
 			MathTest.input.selectAll();
-		}
-		else {
+		}else if(txt.toLowerCase().equals("elementary") && MathTest.state == MathTest.DIFFICULTY_CHANGING_STATE){
+			MathTest.setDifficulty(MathTest.ELEMENTARY_DIFFICULTY);
+			MathTest.EnterText("Starting a new game set in the Elementary Difficulty.");
+			MathTest.numberOfTimesPlayed = 1;
+			MathTest.EnterText("Question #" + MathTest.numberOfTimesPlayed);
+			MathTest.number1 = MathTest.generator.nextInt(11);
+			MathTest.number2 = MathTest.generator.nextInt(11);
+			MathTest.total = MathTest.number1 + MathTest.number2;
+			MathTest.input.requestFocusInWindow();
+			MathTest.input.selectAll();
+			MathTest.EnterText("What is " + MathTest.number1 + " + " + MathTest.number2 + "?");
+		}else if(txt.toLowerCase().equals("middle school") && MathTest.state == MathTest.DIFFICULTY_CHANGING_STATE){
+			MathTest.setDifficulty(MathTest.MIDDLE_SCHOOL_DIFFICULTY);
+			MathTest.EnterText("Starting a new game set in the Middle School Difficulty.");
+			MathTest.numberOfTimesPlayed = 1;
+			MathTest.EnterText("Question #" + MathTest.numberOfTimesPlayed);
+			MathTest.number1 = MathTest.generator.nextInt(11);
+			MathTest.number2 = MathTest.generator.nextInt(11);
+			MathTest.total = MathTest.number1 + MathTest.number2;
+			MathTest.EnterText("What is " + MathTest.number1 + " + " + MathTest.number2 + "?");
+			MathTest.input.requestFocusInWindow();
+			MathTest.input.selectAll();
+		}else if(txt.toLowerCase().equals("high school") && MathTest.state == MathTest.DIFFICULTY_CHANGING_STATE){
+			MathTest.setDifficulty(MathTest.HIGH_SCHOOL_DIFFICULTY);
+			MathTest.EnterText("Starting a new game set in the High School Difficulty.");
+			MathTest.numberOfTimesPlayed = 1;
+			MathTest.EnterText("Question #" + MathTest.numberOfTimesPlayed);
+			MathTest.number1 = MathTest.generator.nextInt(11);
+			MathTest.number2 = MathTest.generator.nextInt(11);
+			MathTest.total = MathTest.number1 + MathTest.number2;
+			MathTest.EnterText("What is " + MathTest.number1 + " + " + MathTest.number2 + "?");
+			MathTest.input.requestFocusInWindow();
+			MathTest.input.selectAll();
+		}else {
 			try{ 
 				int inputValue = Integer.parseInt(MathTest.input.getText());
 				MathTest.nextQuestion(MathTest.number1, MathTest.number2, inputValue);
